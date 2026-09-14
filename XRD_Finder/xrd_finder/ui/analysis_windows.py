931fbec56415335ba242d6d3fb05618e7f70285d
ABKuznetsov <ku.artemy@gmail.com>
2026-09-14 08:25:46 +0700

    refactor: make Finder distribution standalone

--- XRD_Finder/xrd_finder/ui/analysis_windows.py
+++ XRD_Finder/xrd_finder/ui/analysis_windows.py
@@ -115,7 +115,6 @@ from xrd_finder.ui.project_tree import ProjectTree
 from xrd_finder.ui.reference_preview_renderer import draw_pdf2_reference, draw_rruff_reference
 from xrd_finder.ui.selected_phases_actions import PhaseFinderSelectedPhasesActionsMixin
 from xrd_finder.ui.structure_overlay import draw_structure_overlay, prepare_structure_overlay
-from xrd_finder.ui.toolkit_catalog_actions import PhaseFinderToolkitCatalogActionsMixin
 from xrd_finder.ui.theme import is_dark_theme, window_style
 from xrd_finder.ui.xrd_plot import create_xrd_plot_widget
 from xrd_finder.ui.analysis_preview import capture_analysis_preview
@@ -732,7 +731,6 @@ class PhaseFinderWindow(
     PhaseFinderPlotActionsMixin,
     PhaseFinderCandidateSearchActionsMixin,
     PhaseFinderDatabaseActionsMixin,
-    PhaseFinderToolkitCatalogActionsMixin,
     AnalysisWindow,
 ):
     def __init__(self, project: Project, *, defer_initial_plot: bool = False) -> None:
@@ -756,7 +754,6 @@ class PhaseFinderWindow(
             refresh_gain=self._schedule_candidate_gain_ranking,
             should_autozoom=self._should_autozoom_match_profile,
         )
-        self._schedule_toolkit_announcement()
 
     def _init_filter_state(self) -> None:
         self.element_table: PeriodicTableWidget | None = None
